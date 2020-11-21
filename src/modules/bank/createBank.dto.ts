3679c67407242c468b3c92f3a543c05d477c815a
Guilherme Scholz Ramos <gramos@acessom.com>
2020-11-21 00:32:17 -0300

    define body

--- src/modules/bank/createBank.dto.ts
+++ src/modules/bank/createBank.dto.ts
@@ -1,9 +1,12 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBankDto {
     @IsNotEmpty()
+    @ApiProperty()
     name: string;
 
     @IsNotEmpty()
+    @ApiProperty()
     compe: string;
 }
